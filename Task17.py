c0e945b23cc2747788b2711faa0f959d2f10d923
Alina <alinakislowa98@gmail.com>
2023-03-25 10:22:22 +0700

    добавлены задачи 16-18

---
+++ Task17.py
@@ -0,0 +1,6 @@
+# Дан список чисел. Определите, сколько в нем 
+# встречается различных чисел.
+import random
+my_list = [random.randint(0,10) for i in range(10)]
+print(my_list)
+print(len(set(my_list)))
